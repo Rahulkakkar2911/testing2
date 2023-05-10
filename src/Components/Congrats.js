016f1f03fd7f363a19d19caca1c5f5ee3a77e532
Rahul Kakkar <kakkar2000rahul@gmail.com>
2023-05-10 18:51:27 +0530

    added congrats component

---
+++ src/Components/Congrats.js
@@ -0,0 +1,22 @@
+//recieve the success state as a prop
+import React from "react";
+const Congrats = (props) => {
+    if(props.success){
+        return (
+            <div data-test='component-congrats'>
+                <span data-test='congrats-message'>
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div data-test='component-congrats'/>
+        )
+    }
+    
+    
+}
+
+export default Congrats;
